552abc40a491fdffd865b0162c3f00a29c55ecc8
Laercio Bernardo <laercio.bernardo@intrapoint.com>
2019-08-22 15:23:57 -0300

    Created module to save data to file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data.js');
 
 //Instantiate the HTTP server
 const httpServer = http.createServer(function(req, res){
