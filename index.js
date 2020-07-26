6742e302897af366f7b5d2d944ee6fd9831cbaf2
Laercio Bernardo <laercio.bernardo@intrapoint.com>
2020-07-26 18:39:49 -0300

    Finish Background workers to notify via sms when checks status was changed

--- index.js
+++ index.js
@@ -1,114 +1,26 @@
-//Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+/*
+ * Primary file for the API
+ *
+ */
 
-//@TODO GET RID OF THIS
-helpers.sendTwilioSms('85984159666', 'Hello!', function(err){
-   console.log('This was the error');
-   console.log(err);
-})
+// Dependencies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-//Instantiate the HTTP server
-const httpServer = http.createServer(function(req, res){
-   unifiedServer(req, res);
-});
+//Declare the app
+const app = {};
 
-//Start the HTTP server
-httpServer.listen(config.httpPort, function(){
-   console.log(`The server is listening on port ${config.httpPort}`);
-});
-
-//Config HTTPS server
-const httpsServerOptions = {
-   'key': fs.readFileSync('./https/key.pem'),
-   'cert': fs.readFileSync('./https/cert.pem')
+//Init function
+app.init = function(){
+   //Start the server
+   server.init();
+   // Start the workers
+   workers.init();
 };
 
-//Instantiate https server
-const httpsServer = https.createServer(httpsServerOptions, function(req, res){
-   unifiedServer(req, res);
-});
-
-//Start the HTTPS server
-httpsServer.listen(config.httpsPort, function(){
-   console.log(`The server is listening on port ${config.httpsPort}`);
-});
-
-//All the server logic for both the http and https server
-
-const unifiedServer = function(req, res){
-   //Get the URL and parse it
-   const parsedUrl = url.parse(req.url, true);
-
-   //Get the path
-   const path = parsedUrl.pathname;
-   const trimmedPath = path.replace(/^\/+|\/$/g,'');
-
-   //Get the query string as an object
-   const queryStringObject = parsedUrl.query;
-
-   //Get the HTTP Method
-   const method = req.method.toLowerCase();
-
-   //Get the headers as an object
-   const headers = req.headers;
-
-   //Get the payload, if any
-   const decoder = new StringDecoder('utf-8');
-   let buffer = '';
-   req.on('data', function(data){
-      buffer += decoder.write(data);
-   });
-
-   req.on('end',function(){
-      buffer += decoder.end();
-
-      //Choose the handler this request should go to. If one is not found, use the notFount handler
-      const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-      //Construct the data object to send to handler
-      const data = {
-         'trimmedPath': trimmedPath,
-         'queryStringObject': queryStringObject,
-         'method': method,
-         'headers': headers,
-         'payload': helpers.parseJsonToObject(buffer)
-      };
-
-      //Route the request to the handler specified in the router
-      chosenHandler(data, function(statusCode, payload){
-         //Use the status code called back by the handler, or default to 200
-         statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
-
-         //Use the payload called back by the handler, or dafult to empty object
-         payload = typeof(payload) === 'object' ? payload : {};
-
-         //Convert the payload to a string
-         const payloadString = JSON.stringify(payload);
-
-         //Return the response
-         res.setHeader('Content-Type', 'application/json');
-         res.writeHead(statusCode);
-         res.end(payloadString);
-
-         //Log the request path
-         console.log("Returning this response: ",statusCode, payloadString);
-      })
-   })
-};
+//Execute
+app.init();
 
 
-//Define a request router
-let router = {
-   'ping': handlers.ping,
-   'hello': handlers.hello,
-   'users': handlers.users,
-   'tokens': handlers.tokens,
-   'checks': handlers.checks
-}
+//Export the app
+module.exports = app;
